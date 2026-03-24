fc314cd7ec2f218665a05e246e3a1827f35598a0
d4wn <qd4wn@outlook.com>
2026-03-24 21:51:31 +0800

    Part 2: django 数据模型定义与操作 & 后台管理页面

--- mysite/settings.py
+++ mysite/settings.py
@@ -37,6 +37,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    "polls.apps.PollsConfig",
+
 ]
 
 MIDDLEWARE = [
@@ -104,7 +107,8 @@ AUTH_PASSWORD_VALIDATORS = [
 
 LANGUAGE_CODE = 'en-us'
 
-TIME_ZONE = 'UTC'
+#TIME_ZONE = 'UTC'
+TIME_ZONE = 'Asia/Shanghai'
 
 USE_I18N = True
 
