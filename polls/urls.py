2f742e7d4eeb5fcd677dfe5501e0edf3804bd687
d4wn <qd4wn@outlook.com>
2026-03-25 21:45:02 +0800

    Part 4: 表单提交, 数据库修改和通用视图

--- polls/urls.py
+++ polls/urls.py
@@ -6,11 +6,17 @@ app_name = "polls"
 # 在启动的 "http://{ip}:{port}" + "{urlpatterns}"
 urlpatterns = [
     # ex: /polls/
-    path("", views.index, name="index"),
+    # path("", views.index, name="index"),
     # ex: /polls/5/
-    path("<int:question_id>/", views.detail, name="detail"),
-    # ex: /polls/5/results/
-    path("<int:question_id>/results/", views.results, name="results"),
+    # path("<int:question_id>/", views.detail, name="detail"),
+    # # ex: /polls/5/results/
+    # path("<int:question_id>/results/", views.results, name="results"),
+    
+    # 修改为通用视图
+    path("", views.IndexView.as_view(), name="index"),
+    path("<int:pk>/", views.DetailView.as_view(), name="detail"),
+    path("<int:pk>/results/", views.ResultsView.as_view(), name="results"),
+    
     # ex: /polls/5/vote/
     path("<int:question_id>/vote/", views.vote, name="vote"),
 
