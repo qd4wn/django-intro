a7455db29cdacec6727680945a825d514e5a4ee3
d4wn <qd4wn@outlook.com>
2026-03-26 18:44:56 +0800

    Part 7: Django 自带 admin 后台的修改

--- polls/models.py
+++ polls/models.py
@@ -2,6 +2,7 @@ import datetime
 
 from django.db import models
 from django.utils import timezone
+from django.contrib import admin
 
 class Question(models.Model):
     question_text = models.CharField(max_length=200)
@@ -9,7 +10,12 @@ class Question(models.Model):
 
     def __str__(self):
         return self.question_text
-
+    # @装饰器：对应 admin 后台页显示 UI、以及排序
+    @admin.display(
+        boolean=True,
+        ordering="pub_date",
+        description="Published recently?",
+    )
     # 判断问题是否在一天内发布
     def was_published_recently(self):
         # bug: 如果是未来的时间也会判断是在最近发布，比如30天后的时间
